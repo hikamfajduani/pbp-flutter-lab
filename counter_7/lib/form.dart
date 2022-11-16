e5f851d1a0dd14a840b62d0c739e5528dca45861
hikam.fajduani <hikam.fajduani@ui.ac.id>
2022-11-17 02:30:41 +0700

    Menambahkan file baru

--- counter_7/lib/form.dart
+++ counter_7/lib/form.dart
@@ -9,6 +9,11 @@ class MyFormPage extends StatefulWidget {
 }
 
 class _MyFormPageState extends State<MyFormPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _namaLengkap = "";
+  String _judul = "";
+  String tipeBudget = 'Pemasukan';
+  List<String> listTipeBudget = ['Pemasukan', 'Pengeluaran'];
     @override
     Widget build(BuildContext context) {
         return Scaffold(
@@ -43,13 +48,115 @@ class _MyFormPageState extends State<MyFormPage> {
                   ],
                 ),
               ),
-            body: Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                        Text('Hello World!'),
-                    ],
+            body: Form(
+              key: _formKey,
+              child: SingleChildScrollView(
+                child: Container(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                  children: [
+                      Padding(
+                          // Menggunakan padding sebesar 8 pixels
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                              decoration: InputDecoration(
+                                  hintText: "Contoh: Beli FIFA 23",
+                                  labelText: "Judul",
+                                  // Menambahkan icon agar lebih intuitif
+                                  icon: const Icon(Icons.people),
+                                  // Menambahkan circular border agar lebih rapi
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(5.0),
+                                  ),
+                              ),
+                              // Menambahkan behavior saat nama diketik 
+                              onChanged: (String? value) {
+                                  setState(() {
+                                      _namaLengkap = value!;
+                                  });
+                              },
+                              // Menambahkan behavior saat data disimpan
+                              onSaved: (String? value) {
+                                  setState(() {
+                                      _namaLengkap = value!;
+                                  });
+                              },
+                              // Validator sebagai validasi form
+                              validator: (String? value) {
+                                  if (value == null || value.isEmpty) {
+                                      return 'Nama lengkap tidak boleh kosong!';
+                                  }
+                                  return null;
+                              },
+                          ),
+                      ),
+                      Padding(
+                          // Menggunakan padding sebesar 8 pixels
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                              decoration: InputDecoration(
+                                  hintText: "Contoh: 1000000",
+                                  labelText: "Nominal",
+                                  // Menambahkan icon agar lebih intuitif
+                                  icon: const Icon(Icons.people),
+                                  // Menambahkan circular border agar lebih rapi
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(5.0),
+                                  ),
+                              ),
+                              // Menambahkan behavior saat nama diketik 
+                              onChanged: (String? value) {
+                                  setState(() {
+                                      _judul = value!;
+                                  });
+                              },
+                              // Menambahkan behavior saat data disimpan
+                              onSaved: (String? value) {
+                                  setState(() {
+                                      _judul = value!;
+                                  });
+                              },
+                              // Validator sebagai validasi form
+                              validator: (String? value) {
+                                  if (value == null || value.isEmpty) {
+                                      return 'Judul tidak boleh kosong!';
+                                  }
+                                  return null;
+                              },
+                          ),
+                      ),
+                      DropdownButtonHideUnderline(
+                        child: DropdownButton(
+                            hint: Text('Pilih jenis'),
+                            items: listTipeBudget.map((String items) {
+                              return DropdownMenuItem(
+                                value: items,
+                                child: Text(items),
+                              );
+                            }).toList(),
+                            value: tipeBudget,
+                            onChanged: (newValue) {
+                              setState(() {
+                                tipeBudget = newValue!;
+                              });
+                            }),
+                      ),
+                      TextButton(
+                        child: const Text(
+                          "Simpan",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(Colors.red),
+                        ),
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {}
+                        },
+                      ),
+                  ],
+                  ),
                 ),
+              ),
             ),
         );
     }
